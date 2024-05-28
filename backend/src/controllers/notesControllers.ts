bf3b25567c102ad81c4228a1257c70f8a330f875
Mohit Sharma <mohit94596@gmail.com>
2024-05-28 15:36:53 +0530

    Notes routes created

--- backend/src/controllers/notesControllers.ts
+++ backend/src/controllers/notesControllers.ts
@@ -51,3 +51,59 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
         next(error)
     }
 }
+
+
+interface UpdateNoteParam{
+    noteId: string
+}
+interface UpdateNoteBody{
+    title?: string, 
+    text?: string
+}
+
+export const updateNote: RequestHandler<UpdateNoteParam, unknown, UpdateNoteBody, unknown> = async (req, res, next) => {
+    const noteId = req.params.noteId
+    const newTitle = req.body.title
+    const newText = req.body.text
+
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "Invalid NoteId")
+        }
+        if(!newTitle){
+            throw createHttpError(400, "Note must have a title")
+        }
+
+        const note = await NoteModel.findById(noteId).exec()
+        
+        if(!note){
+            throw createHttpError(404, "Note not found")
+        }
+
+        note.title = newTitle
+        note.text = newText
+        const updatedNote = await note.save()
+        res.status(200).json(updatedNote)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId
+
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "Invalid NoteId")
+        }
+        const note = await NoteModel.findById(noteId).exec()
+        if(!note){
+            throw createHttpError(404, "Note not found")
+        }
+        await NoteModel.findByIdAndDelete(noteId)
+        res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+}
--- backend/src/routes/notesRoutes.ts
+++ backend/src/routes/notesRoutes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get('/', notesControllers.getNotes);
 router.get('/:noteId', notesControllers.getSingleNote);
 router.post('/', notesControllers.createNote);
+router.patch('/:noteId', notesControllers.updateNote)
+router.delete('/:noteId', notesControllers.deleteNote)
 
 export default router; 
