4d9f1b7f8d6567ab64554e87a2c97bfd5bad8502
Mohit Sharma <mohit94596@gmail.com>
2024-06-04 20:28:58 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,8 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://cool-notes-app-sigma.vercel.app',
+    //origin: 'https://cool-notes-app-sigma.vercel.app',
+    origin: 'http://localhost:3000/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
 }));
