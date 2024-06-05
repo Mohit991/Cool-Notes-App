c1bcbcb80cf189e634a8dcb42226afe0004631ee
Mohit Sharma <mohit94596@gmail.com>
2024-06-05 15:40:25 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,9 +26,9 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 60 * 60 * 1000, // 1 hour
-        // httpOnly: true,
-        // secure: true, // true in production, false otherwise
-        sameSite: 'none' // None in production, Lax otherwise
+        httpOnly: true,
+        secure: true, // true in production, false otherwise
+        // sameSite: 'none' // None in production, Lax otherwise
     },
     rolling: true,
     store: MongoStore.create({
