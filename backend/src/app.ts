7ef8265688d2342214f7861701e3b9c42eed18b6
Mohit <mohit94596@gmail.com>
2024-06-05 15:19:15 +0530

    Prod Changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,59 +1,57 @@
-import "dotenv/config"
-import morgan from 'morgan'
+import "dotenv/config";
+import morgan from 'morgan';
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notesRoutes';
-import userRoutes from './routes/userRoutes'
+import userRoutes from './routes/userRoutes';
 import createHttpError, { isHttpError } from "http-errors";
 import session from "express-session";
-import env from './util/validateEnv'
+import env from './util/validateEnv';
 import MongoStore from "connect-mongo";
 import { requiresAuth } from "./middleware/auth";
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 
-
-// app.use(cors())
-
-
 app.use(cors({
-    // origin: 'https://cool-notes-app-sigma.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://cool-notes-app-sigma.vercel.app',
+    // origin: 'http://localhost:3000',
     credentials: true
 }));
 
-
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.use(session({
     secret: env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 60*60*1000
+        maxAge: 60 * 60 * 1000, // 1 hour
+        httpOnly: true,
+        secure: true, // true in production, false otherwise
+        sameSite: 'none' // None in production, Lax otherwise
     },
     rolling: true,
     store: MongoStore.create({
         mongoUrl: env.MONGODB_CONNECTION_STRING
     })
-}))
+}));
 
-app.use("/api/user", userRoutes)
-app.use("/api/notes", requiresAuth, notesRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/notes", requiresAuth, notesRoutes);
 
 app.use((req, res, next) => {
-    next(createHttpError(404, "Endpoint not found!"))
-}) 
+    next(createHttpError(404, "Endpoint not found!"));
+});
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.log(error);
-    let errorMessage = "Unknown Error Occurred!"
-    let statusCode = 500
-    if(isHttpError(error)){
-        statusCode = error.status
-        errorMessage = error.message
+    let errorMessage = "Unknown Error Occurred!";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
     }
-    res.status(statusCode).json({error: errorMessage})
-})
+    res.status(statusCode).json({ error: errorMessage });
+});
 
-export default app
+export default app;
